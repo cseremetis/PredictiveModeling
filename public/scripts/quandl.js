ea7e7cf43fd991c46a2e508750c1d919ab070862
Christian Seremetis <christianseremetis@gmail.com>
2018-08-07 08:59:07 -0400

    Meddling with spaces

--- public/scripts/quandl.js
+++ public/scripts/quandl.js
@@ -3,27 +3,27 @@
  	*/
 define(["jquery"], function(jquery) {
 
-	let params = {
-		start_date: "2008-01-01",
-		end_date: "2018-07-26",
-		column_index: 1,
-		collapse: "quarterly"
-	};
+  let params = {
+    start_date: "2008-01-01",
+    end_date: "2018-07-26",
+    column_index: 1,
+    collapse: "quarterly"
+  };
 
-	const API_KEY = "yQK7NuEPvshM5dEE_nu5";
-	const HOST = "https://www.quandl.com/api/v3/datasets";
+  const API_KEY = "yQK7NuEPvshM5dEE_nu5";
+  const HOST = "https://www.quandl.com/api/v3/datasets";
 
-	return {
+  return {
 
-		/** Uses the JQuery get() function to return
-		  * info from the Quandl API.
-			*/
-			getInfo: function(database, query, _callback) {
-				let url = `${HOST}/${database}/${query}.json?${$.param(params)}&api_key=${API_KEY}`;
+    /** Uses the JQuery get() function to return
+      * info from the Quandl API.
+      */
+      getInfo: function(database, query, _callback) {
+        let url = `${HOST}/${database}/${query}.json?${$.param(params)}&api_key=${API_KEY}`;
 
-				$.get(url, function(response) {
-					_callback(response.dataset.data);
-				});
-			}
-	 }
+        $.get(url, function(response) {
+          _callback(response.dataset.data);
+        });
+      }
+   }
 });
